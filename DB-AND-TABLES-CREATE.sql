278ddef6af5e2897a7108987d7f026e9953b3d4d
Rahamathullah <rahamath18@yahoo.com>
2017-09-09 23:51:12 +0530

    1st commit

---
+++ DB-AND-TABLES-CREATE.sql
@@ -0,0 +1,9 @@
+
+create database as springwithhibernate;
+
+CREATE TABLE `springwithhibernate`.`Person` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL DEFAULT '',
+  `country` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
---
+++ README.txt
@@ -0,0 +1,11 @@
+
+
+org.springframework-version: 4.0.3.RELEASE
+		
+hibernate.version: 4.3.5.Final
+
+For details pls check …/web-inf/lib/
+
+access following url
+
+	http://localhost:8080/SpringMVCWithHibernate/persons
